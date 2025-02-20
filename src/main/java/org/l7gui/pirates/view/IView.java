7490820a8ca1a3ac01d2fd954187a71755207884
guilhem0908 <l7guilhem@gmail.com>
2025-02-20 03:56:28 +0100

    Add new method to IView

--- src/main/java/org/l7gui/pirates/view/IView.java
+++ src/main/java/org/l7gui/pirates/view/IView.java
@@ -3,4 +3,7 @@ package org.l7gui.pirates.view;
 public interface IView {
     void displayWelcome();
     String getPlayerName(int playerNumber);
+    // void displayPlayerState(String playerName, int hearts, int popularity);
+    // void displayPlayerHand(String playerName, String[] hand);
+    // void displayWinner(String winnerName)
 }
